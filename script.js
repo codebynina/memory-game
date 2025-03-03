ffa5fb03875bced5698a2ba0f22ae8c1acca236b
niña <ninamarieondevilla@gmail.com>
2025-03-03 22:30:59 +0100

    Added images on Js

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded",() => {
+    const gameContainer = document.querySelector(".game-container");
+
+    const cards = [
+        {front: "image/front1.jpeg", back: "image/card-back.png"},
+        {front: "image/front2.jpeg", back: "image/card-back.png"},
+        {front: "image/front3.jpeg", back: "image/card-back.png"},
+        {front: "image/front4.jpeg", back: "image/card-back.png"},
+        {front: "image/front5.jpeg", back: "image/card-back.png"},
+        {front: "image/front6.jpeg", back: "image/card-back.png"},
+        {front: "image/front1.jpeg", back: "image/card-back.png"},
+        {front: "image/front2.jpeg", back: "image/card-back.png"},
+        {front: "image/front3.jpeg", back: "image/card-back.png"},
+        {front: "image/front4.jpeg", back: "image/card-back.png"},
+        {front: "image/front5.jpeg", back: "image/card-back.png"},
+        {front: "image/front6.jpeg", back: "image/card-back.png"},
+    ];
+});
